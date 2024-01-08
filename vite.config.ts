343ebdc0ab41da71515a0d32b8f8ccb4084dd53c
krasi1 <krissi.petrov@gmail.com>
2024-01-08 20:39:33 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
   plugins: [react()],
 })
