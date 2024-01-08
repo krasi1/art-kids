a58d0f2131a35df4ebfc2062034e2791a13c8af9
krasi1 <krissi.petrov@gmail.com>
2024-01-08 21:00:10 +0200

    more attempts at fixing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'vite/modulepreload-polyfill'
+
 import App from './App.tsx'
 import './index.css'
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/src/main.tsx',
-    },
-  },
   plugins: [react()],
 })
