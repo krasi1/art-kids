79693730f13f418b6b22926710de88d2e5834252
krasi1 <krissi.petrov@gmail.com>
2024-01-12 01:39:00 +0200

    please

--- src/Statistics.tsx
+++ src/Statistics.tsx
@@ -1,8 +1,8 @@
 const Statistics = () => {
   return (
     <div id="statDiv">
-      <img className="graph" src="assets/Graph1.png" alt="Graph 1" />
-      <img className="graph" id="graph2" src="assets/Graph2.png" alt="Graph 2" />
+      <img className="graph" src="/assets/Graph1.png" alt="Graph 1" />
+      <img className="graph" id="graph2" src="/assets/Graph2.png" alt="Graph 2" />
 
     </div>
   );
