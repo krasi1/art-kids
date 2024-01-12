309ecb069e8348669974b477e6188e31e61857e1
krasi1 <krissi.petrov@gmail.com>
2024-01-12 02:56:42 +0200

    AAAAAAAAAAAAAAAA

--- src/Statistics.tsx
+++ src/Statistics.tsx
@@ -1,11 +1,9 @@
 const Statistics = () => {
   return (
-    //@ts-expect-error stop
     <div id="statDiv">
       <img className="graph" src="/art-kids/assets/Graph1.png" alt="Graph 1" />
       <img
         className="graph"
-        //@ts-expect-error stop
         id="graph2"
         src="/art-kids/assets/Graph2.png"
         alt="Graph 2"
